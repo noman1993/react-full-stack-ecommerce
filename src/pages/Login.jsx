f62a14b25662bb15609030f76ba86aba37ac0aad
noman1993 <chopolnoman07@gmail.com>
2025-04-22 02:34:40 -0700

    complete the login  page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-      login
-    </div>
-  )
-}
+  const [currentState, setCurrentState] = useState("Sign Up");
 
-export default Login
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <form onSubmit={onSubmitHandler} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-700">
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="prata-regular text-3xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+      </div>
+      {currentState === "Login" ? (
+        ""
+      ) : (
+        <input
+          type="text"
+          className="w-full px-3 py-2 border border-gray-800"
+          placeholder="Name"
+          required
+        />
+      )}
+      <input
+        type="email"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Email"
+        required
+      />
+      <input
+        type="password"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Password"
+        required
+      />
+      <div className="w-full flex justify-between text-sm mt-[-8px]">
+        {
+          currentState === 'Login' ? <p className="cursor-pointer">forgot password?</p> : ''
+        }
+        {currentState === "Login" ? (
+          <p
+            onClick={() => setCurrentState("Sign Up")}
+            className="cursor-pointer"
+          >
+            Create Account
+          </p>
+        ) : (
+          <p
+            onClick={() => setCurrentState("Login")}
+            className="cursor-pointer"
+          >
+            Login
+          </p>
+        )}
+      </div>
+
+      <button className="bg-black text-white font-light px-8 py-2 mt-4">{currentState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
+    </form>
+  );
+};
+
+export default Login;
